c33f9cbbc388d2758aa8c5231fdaeb09973a83a9
Andrestabango <andres.tabango@udla.edu.ec>
2024-06-03 11:20:06 -0500

    Initial commit

---
+++ src/Lista.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Lista {
+
+
+    private List<Persona> personas;
+    public Lista() {
+        personas = new ArrayList<Persona>();
+    }
+
+    public List<Persona> getPersonas() {
+        return personas;
+    }
+
+    public void adicionarElementos(Persona p) throws Exception {
+        int count = 0;
+        for (Persona pa : personas) {
+            if (pa.getId().equals(p.getId())) {
+                count++;
+            }
+        }
+        if (count >= 2) {
+            throw new Exception("Persona con este ID de banner ya existe más de dos veces");
+        }
+
+        personas.add(p);
+    }
+
+    public List<Persona> listarPersonas(){
+        List<Persona> lista = new LinkedList<>();
+        for (Persona e: personas)
+            lista.add(e);
+        return lista;
+    }
+
+
+    public Persona buscarPersona(String id) {
+        for (Persona p : personas)
+            if (p.getId().equals(id))
+                return p;
+        return null;
+    }
+    public void editar(String nuevoNombre, String id, String nuevoTipoPersona, Vehiculo nuevaPlaca, Vehiculo nuevoTipoVehiculo)  throws Exception {
+        Persona persona = buscarPersona(id);
+        if (persona != null) {
+            persona.setNombre(nuevoNombre);
+            persona.setTipoPersona(nuevoTipoPersona);
+            persona.setVehiculo(nuevaPlaca);
+            persona.setVehiculo(nuevoTipoVehiculo);
+        } else {
+            throw new Exception("No se encontró ninguna perona con el id : " + id);
+        }
+    }
+
+
+
+
+}
